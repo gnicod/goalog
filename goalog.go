28d9ea70c6f39a66aac8e3361a5ed63af4379a94
ovski <ovskywalker@gmail.com>
2013-05-23 02:45:37 +0200

    can colorize PHP error message

--- goalog.go
+++ goalog.go
@@ -14,41 +14,45 @@ type Color struct {
 	HEADER , OKBLUE , OKGREEN , WARNING , FAIL , ENDC string
 }
 
-var color     = Color{"\033[95m","\033[94m","\033[92m", "\033[93m","\033[91m","\033[0m"}
-var logfile   = "/var/log/httpd/error_log"
-var lineRegex = regexp.MustCompile("\\[(.*?)\\] \\[(.*?)\\] \\[(.*?)\\] (.*)")
+var color     = Color{"\033[1m\033[95m","\033[1m\033[94m","\033[1m\033[92m", "\033[1m\033[93m","\033[1m\033[91m","\033[0m"}
 
-func cprint(colorName string,text string) {
-	pr := colorName + text + color.ENDC
-	fmt.Println(pr)
+var lineErrorRegex = regexp.MustCompile("\\[(.*?)\\] \\[error\\] \\[(.*?)\\] (.*)")
+var lineWarnRegex = regexp.MustCompile("\\[(.*?)\\] \\[warn\\] (.*)")
+var lineNoticeRegex = regexp.MustCompile("\\[(.*?)\\] \\[notice\\] (.*)")
+
+var msgParseErrorRegex = regexp.MustCompile("(.*?):(.*) in (.*) on (line [0-9]{1,}), referer: (.*)")
+
+func colorize(colorName string,text string) string {
+	return colorName + text + color.ENDC
 }
 
-func colorize_pattern(colorName string ,pattern string ,str string) string{
-	src      := []byte(str)
-	search   := regexp.MustCompile(pattern)
-	colorN   := []byte(colorName)
-	endcolor  := []byte(color.ENDC)
-	i := -1
-	src = search.ReplaceAllFunc(src, func(s []byte) []byte {
-		if i != 0 {
-			i -= 1
-			var tmp = append(colorN,s...)
-			return append(tmp,endcolor...)
-		}
-		return s
-	})
-	return (string(src))
-}
-
-func parse_line(input string) (string,string,string,string){
-	if lineRegex.MatchString(input) {
-		splitt      := lineRegex.FindAllStringSubmatch(input,-1)[0]
-		return splitt[1],splitt[2],splitt[3],splitt[4]
+func get_colorized_msg(input string) string{
+	if msgParseErrorRegex.MatchString(input) {
+		splitt      := msgParseErrorRegex.FindStringSubmatch(input)
+		return colorize(color.WARNING,splitt[1]) +":" +splitt[2]+" \n\t=> "+colorize(color.OKBLUE,splitt[3])+" on "+colorize(color.OKBLUE,splitt[4])+"\n\t"+splitt[5]
 	}
-	return "","","",""
+	return input
 }
 
-func get_last_line() string{
+func parse_line(input string) {
+	//Line [error]
+	if lineErrorRegex.MatchString(input) {
+		splitt      := lineErrorRegex.FindAllStringSubmatch(input,-1)[0]
+		fmt.Printf("[%s] %s\n",colorize(color.FAIL,"error"),get_colorized_msg(splitt[3]))
+	}
+	//Line [warn]
+	if lineWarnRegex.MatchString(input) {
+		splitt      := lineWarnRegex.FindAllStringSubmatch(input,-1)[0]
+		fmt.Printf("[%s] %s \n",colorize(color.WARNING,"warn"),splitt[2])
+	}
+	//Line [notice]
+	if lineNoticeRegex.MatchString(input) {
+		splitt      := lineNoticeRegex.FindAllStringSubmatch(input,-1)[0]
+		fmt.Printf("[%s]  %s\n",colorize(color.HEADER,"notice"),splitt[2])
+	}
+}
+
+func get_last_line(logfile string) string{
 	f, err := os.Open(logfile)
 	if err != nil {
 		log.Fatal(err)
@@ -70,10 +74,12 @@ func get_last_line() string{
 			log.Fatal("Error: Unexpected long line reading", f.Name())
 		}
 	}
-	return string("aa ")
+	return string("")
 }
 
 func main() {
+	logfile := os.Args[1]
+	fmt.Println("Watching ",logfile)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -84,15 +90,11 @@ func main() {
 			select {
 			case ev := <-watcher.Event:
 				if(ev.IsModify()){
-					var line = get_last_line()
-					da,ty,cl,msg := parse_line(line)
-					fmt.Println(msg)
-					fmt.Println(ty)
-					fmt.Println(cl)
-					fmt.Println(da)
+					var line = get_last_line(logfile)
+					parse_line(line)
 				}
 			case err := <-watcher.Error:
-				log.Println("error:", err)
+				log.Fatal("error:", err)
 			}
 		}
 	}()
